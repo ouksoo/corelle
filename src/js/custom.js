6cd96579f6dce602b124e26844fa56714e79d766
eoksoo Kim <>
2018-09-30 21:21:37 +0900

    pc 개발 마무리중

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,10 +1,24 @@
+let appendMainmenu = '';
+appendMainmenu += '<h1>Corelle Brands</h1>';
+appendMainmenu += '<nav>';
+appendMainmenu += '    <ul class="clearfix">';
+appendMainmenu += '        <li><a href="#">About Us</a></li>';
+appendMainmenu += '        <li><a href="#">Brands</a></li>';
+appendMainmenu += '        <li><a href="product_corelle.html">Product</a></li>';
+appendMainmenu += '        <li><a href="support_faq.html">Support</a></li>';
+appendMainmenu += '        <li><a href="news-list.html">News & Event</a></li>';
+appendMainmenu += '        <li><a href="#">Webzine</a></li>';
+appendMainmenu += '    </ul>';
+appendMainmenu += '    <a href="#">Online-shop <span>바로가기</span></a>';
+appendMainmenu += '</nav>';
+
 let appendSubmenu = '';
 appendSubmenu += '<div class="nav-sub clearfix">';
 appendSubmenu += '  <div class="show-submenu clearfix">';
 appendSubmenu += '      <ul>';
 appendSubmenu += '          <li><a href="#">코렐 브랜드 소개/역사</a></li>';
-appendSubmenu += '          <li><a href="#">회사위치및 정보</a></li>';
-appendSubmenu += '          <li><a href="#">홍보영상 및 보도자료</a></li>';
+appendSubmenu += '          <li><a href="about_contact.html">회사위치및 정보</a></li>';
+appendSubmenu += '          <li><a href="news-list.html">홍보영상 및 보도자료</a></li>';
 appendSubmenu += '      </ul>';
 appendSubmenu += '      <ul class="brands">';
 appendSubmenu += '          <li><a href="#">Corelle</a></li>';
@@ -18,17 +32,17 @@ appendSubmenu += '          <li><span class="line"></span></li>';
 appendSubmenu += '          <li><a href="#">OXO</a></li>';
 appendSubmenu += '      </ul>';
 appendSubmenu += '      <ul>';
-appendSubmenu += '          <li><a href="#">제품소개</a></li>';
+appendSubmenu += '          <li><a href="product_corelle.html">제품소개</a></li>';
 appendSubmenu += '      </ul>';
 appendSubmenu += '      <ul>';
-appendSubmenu += '          <li><a href="#">자주묻는 질문</a></li>';
-appendSubmenu += '          <li><a href="#">1:1문의</a></li>';
-appendSubmenu += '          <li><a href="#">품질보증 및 보관방법</a></li>';
-appendSubmenu += '          <li><a href="#">매장안내</a></li>';
+appendSubmenu += '          <li><a href="support_faq.html">자주묻는 질문</a></li>';
+appendSubmenu += '          <li><a href="support_asking.html">1:1문의</a></li>';
+appendSubmenu += '          <li><a href="support_caution.html">품질보증 및 보관방법</a></li>';
+appendSubmenu += '          <li><a href="support_store.html">매장안내</a></li>';
 appendSubmenu += '      </ul>';
 appendSubmenu += '      <ul>';
-appendSubmenu += '          <li><a href="#">새로운 소식</a></li>';
-appendSubmenu += '          <li><a href="#">이벤트</a></li>';
+appendSubmenu += '          <li><a href="news-list.html">새로운 소식</a></li>';
+appendSubmenu += '          <li><a href="event-list.html">이벤트</a></li>';
 appendSubmenu += '      </ul>';
 appendSubmenu += '      <ul class="webzine">';
 appendSubmenu += '          <li class="s"><a href="#">테이블 텔링</a></li>';
@@ -38,6 +52,70 @@ appendSubmenu += '      </ul>';
 appendSubmenu += '  </div>';
 appendSubmenu += '</div>';
 
+let appendFooter = '';
+appendFooter += '<div class="sitemap clearfix">';
+appendFooter += '    <ul>';
+appendFooter += '        <li class="f">About Us</li>';
+appendFooter += '        <li><a href="#">코렐 브랜드 소개/역사</a></li>';
+appendFooter += '        <li><a href="about_contact.html">회사위치및 정보</a></li>';
+appendFooter += '        <li><a href="news-list.html">홍보영상 및 보도자료</a></li>';
+appendFooter += '    </ul>';
+appendFooter += '    <ul class="brands">';
+appendFooter += '        <li class="f">Brands</li>';
+appendFooter += '        <li><a href="#">Corelle</a></li>';
+appendFooter += '        <li><a href="#">Pyrex</a></li>';
+appendFooter += '        <li><a href="#">Vision</a></li>';
+appendFooter += '        <li><a href="#">Corningware</a></li>';
+appendFooter += '        <li><a href="#">Snapware</a></li>';
+appendFooter += '        <li><a href="#">Chicaco Cutlery</a></li>';
+appendFooter += '        <li><a href="#">Corelle cookware</a></li>';
+appendFooter += '        <li><span class="line"></span></li>';
+appendFooter += '        <li><a href="#">OXO</a></li>';
+appendFooter += '    </ul>';
+appendFooter += '    <ul>';
+appendFooter += '        <li class="f">Product</li>';
+appendFooter += '        <li><a href="product_corelle.html">제품소개</a></li>';
+appendFooter += '    </ul>';
+appendFooter += '    <ul>';
+appendFooter += '        <li class="f">Support</li>';
+appendFooter += '        <li><a href="support_faq.html">자주묻는 질문</a></li>';
+appendFooter += '        <li><a href="support_asking.html">1:1문의</a></li>';
+appendFooter += '        <li><a href="support_caution.html">품질보증 및 보관방법</a></li>';
+appendFooter += '        <li><a href="support_store.html">매장안내</a></li>';
+appendFooter += '    </ul>';
+appendFooter += '    <ul>';
+appendFooter += '        <li class="f">News & Event</li>';
+appendFooter += '        <li><a href="news-list.html">새로운 소식</a></li>';
+appendFooter += '        <li><a href="event-list.html">이벤트</a></li>';
+appendFooter += '    </ul>';
+appendFooter += '    <ul class="webzine">';
+appendFooter += '        <li class="f">Webzine</li>';
+appendFooter += '        <li class="s"><a href="#">테이블 텔링</a></li>';
+appendFooter += '        <li class="s"><a href="#">마켓스트리트뉴욕</a></li>';
+appendFooter += '        <li class="s"><a href="#">파이렉스의 키친놀로지</a></li>';
+appendFooter += '    </ul>';
+appendFooter += '    <a href="#">Online-shop <span>바로가기</span></a>';
+appendFooter += '</div>';
+appendFooter += '<div class="copyright">';
+appendFooter += '    <h2>Corelle Brands</h2>';
+appendFooter += '    <div class="menus">';
+appendFooter += '        <a href="#">이용약관</a><a href="#">개인정보취급방침</a><a href="#">이메일주소 수집거부</a>';
+appendFooter += '        <p class="f">&copy;2018 Copyright by Corelle Brands, All Rights Reserved.</p>';
+appendFooter += '    </div>';
+appendFooter += '    <div class="phone-time">';
+appendFooter += '        <p>고객센터 02 - 2670 - 7800</p>';
+appendFooter += '        <p>평일 09:00 - 18:00 (점심시간 : 12:00 - 13:30)</p>';
+appendFooter += '    </div>';
+appendFooter += '    <div class="address">';
+appendFooter += '        <p>이메일 : <span class="f">WKKCS@worldkichen.com</span></p>';
+appendFooter += '        <p>주소 : 코렐 브랜드 코리아 - 서울 강남구 테헤란로 98길 8 8층(KT&G 대치타워 8층)</p>';
+appendFooter += '    </div>';
+appendFooter += '    <ul class="sns">';
+appendFooter += '        <li><a href="#" class="facebook">페이스북</a></li>';
+appendFooter += '        <li><a href="#" class="insta">인스타그램</a></li>';
+appendFooter += '        <li><a href="#" class="youtube">유튜브</a></li>';
+appendFooter += '    </ul>';
+appendFooter += '</div>';
 
 let CORE = {
     appSet : {
@@ -45,12 +123,36 @@ let CORE = {
     },
     init : function() {
         //submenu append
+        $('header').html(appendMainmenu);
+        if(typeof thisPage !== 'undefined') {
+            if(thisPage === 'about') {
+                $('header nav li:nth-child(1) a').addClass('here');
+            }
+            else if(thisPage === 'brands') {
+                $('header nav li:nth-child(2) a').addClass('here');
+            }
+            else if(thisPage === 'product') {
+                $('header nav li:nth-child(3) a').addClass('here');
+            }
+            else if(thisPage === 'support') {
+                $('header nav li:nth-child(4) a').addClass('here');
+            }
+            else if(thisPage === 'news') {
+                $('header nav li:nth-child(5) a').addClass('here');
+            }
+            else if(thisPage === 'webzine') {
+                $('header nav li:nth-child(6) a').addClass('here');
+            }
+        }
+
         $('header').append(appendSubmenu);
+        $('footer').html(appendFooter);
 
         this.topMenuIndicator();
         this.naviMouseEvent();
         this.realTabOperation();
         this.frequentlyAskedQuestions();
+        this.productInfoShow();
 
         // custom scrollbar plug
         if($('div.video-detail div.s').length > 0) {
@@ -58,12 +160,8 @@ let CORE = {
                 theme : 'eoksoo'
             });   
         }
-        $('#innerGallery').bjqs({
-            height      : 1024,
-            width       : 1680,
-            responsive  : true
-        });
     },
+
     // GNB indicator init
     topMenuIndicator : function() {
         $('nav a.here').parent().addClass('on');
@@ -71,6 +169,7 @@ let CORE = {
             window.location.href = '/';
         })
     },
+
     // GNB indicator operate
     naviMouseEvent : function() {
         $('nav li').on('mouseenter', function(){
@@ -79,32 +178,37 @@ let CORE = {
             });
             $(this).addClass('on');
             $('header .nav-sub').stop().fadeIn('fast');
+            $('header').addClass('on');
             CORE.appSet.gnbActive = true;
         });
         $('nav li').on('mouseleave', function(){
             $(this).removeClass('on');
+            $('header').removeClass('on');
             CORE.appSet.gnbActive = false;           
             if(!CORE.appSet.gnbActive){
                 $('nav a.here').parent().addClass('on');
                 $('header .nav-sub').stop().fadeOut('fast');
             }
         });
-        $('header .nav-sub').on('mouseover', function(){
+        $('header .nav-sub').on('mouseenter', function(){
             $('nav li').each(function(){
                 $(this).removeClass('on');
             });
-            $('header .nav-sub').stop()
+            $('header .nav-sub').stop().fadeIn('fast');
+            $('header').addClass('on');
             $(this).css('display','block');
         });
-        $('header .nav-sub').on('mouseout', function(){
+        $('header .nav-sub').on('mouseleave', function(){
             $('header .nav-sub').stop()
             $(this).fadeOut();
+            $('header').removeClass('on');
             if(!CORE.appSet.gnbActive){
                 $('nav a.here').parent().addClass('on');
                 $('header .nav-sub').stop().fadeOut('fast');
             }
         });
     },
+
     // TAB plug
     realTabOperation : function() {
         $('.tabsets.real-tab a').on('click', function() {
@@ -113,6 +217,7 @@ let CORE = {
             $(this).addClass('on');
         });
     },
+
     // FAQ
     frequentlyAskedQuestions : function() {
         $('.questions a.question-link').on('click', function(e){
@@ -125,10 +230,22 @@ let CORE = {
             $(this).next('.answer').slideDown(300);
         });
     },
+
     // Article Show
     articleShowGallery : function() {
         $('div.article-area a.show').on('click', function(){
             
+        });
+    },
+
+    // Product info show
+    productInfoShow : function() {
+        $('.sliders div.pd').on('mouseenter', function(){
+            $(this).siblings('.info').fadeIn('fast');
+
+            $('.sliders div.info').on('mouseleave', function(){
+                $('.sliders div.info').fadeOut('fast');
+            })
         });
     }
 }
